9f8f24d69b62f302a55d5205a90ea115611d5cac
HisenXuEdu <2230728@tongji.edu.cn>
2024-01-22 15:33:25 +0800

    change plot

--- util/plot.py
+++ util/plot.py
@@ -1,41 +1,58 @@
 import sys
 from util.dobot_api import DobotApiDashboard, DobotApi, DobotApiMove, MyType, alarmAlarmJsonFile
 import numpy as np
-import time
 from time import sleep
 import pandas as pd
 from visdom import Visdom
-import threading
 
 
-class Plot():
-    def __init__(self,win_size=200,title='data'):
-        self.viz=Visdom()
-        self.data_list=[]
-        self.win=win_size
-        self.title=title
-        self.opt_pose = {
-            "title": self.title,
+opt_pose  = {
+            "title": 'POSE',
             "width":1000,
             "height":500,
             "legend":['x','y','z','r','p','y']
-            
-        }
-        self.opt_force = {
-            "title": self.title,
+            }
+opt_force = {
+            "title": 'FORCE',
             "width":1000,
             "height":500,
             "legend":['f1','f2','f3','f4','f5','f6'],
             "ylim":[0, 50]
-        }
+            }
+opt_circle = {
+            "title": 'DATA',
+            "width":1500,
+            "height":600,
+            "legend":['f1','f2','f3','f4','f5','f6'],
+            "ylim":[0, 50],
+            "markerize":50,
+            "markers":True
+            }
 
 
-    def plot(self,data,opt={}):
+class Plot():
+    def __init__(self,win_size=200,title='DATA',opt={}):
+        self.viz=Visdom()
+        self.data_list=[]
+        self.win=win_size
+        self.title=title
         if not opt:
-            opt=self.opt_pose
+            self.opt={
+            "title": self.title,
+            "width":1000,
+            "height":500,
+            "legend":['x','y','z','r','p','y']
+        }
+        else:
+            self.opt=opt
+        self.opt["title"]=self.title
+
+
+
+    def plot(self,data):
         self.data_list.append(data)
         sleep(0.01)
         if len(self.data_list)>self.win:
             self.data_list.pop(0)
-        self.viz.line(Y=self.data_list, win=self.title, opts=opt)
+        self.viz.line(Y=self.data_list, win=self.title, opts=self.opt)
         pass
