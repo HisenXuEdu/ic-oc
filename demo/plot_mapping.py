c95b04fe9cd324d46829a5bb7ed7f91948cefdb2
HisenXuEdu <2230728@tongji.edu.cn>
2025-02-18 16:37:23 +0800

    绘制速度位置曲线

--- demo/plot_mapping.py
+++ demo/plot_mapping.py
@@ -2,28 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #读取data.csv
-data = pd.read_csv('data.csv')
+data = pd.read_csv('data1.csv')
+data2 = pd.read_csv('data2.csv')
 
-# # 取第一列数据
-# print(data.shape)
-# x=data.iloc[:,0]
-# # 对x进行差分
-# diff = x.diff()
+y1 = data.iloc[:,0]
+y2 = data2.iloc[:,0]
+plt.figure(figsize=(10, 6))
+plt.plot(y1, label='v_mapping')
+plt.plot(y2, label='v')
+plt.legend()
+plt.show()
 
-# # 对x进行差分后的数据进行绘图
-# plt.figure(figsize=(10, 6))
-# plt.plot(diff)
-# plt.title('diff of x')
+y1 = data.iloc[35:250,0]
+# 对y1进行积分
+p1 = y1.cumsum()/80
+a1 = y1.diff()
 
-y = data.iloc[50:350,0]
-a = y.diff()
+y2 = data2.iloc[40:200,0]
+# 对y2进行积分
+p2 = y2.cumsum()/80
+a2 = y2.diff()
 # 对x进行差分后的数据进行绘图
 plt.figure(figsize=(10, 6))
-plt.plot(y)
-plt.plot(a)
-plt.title('diff of x')
+plt.plot(p1, y1, label='v_mapping')
+plt.plot(p1, a1, label='a_mapping')
+plt.plot(p2, y2, label='v')
+plt.plot(p2, a2, label='a')
 
-
+plt.legend()
+plt.xlim(0, 0.6)
 
 
 plt.show()
