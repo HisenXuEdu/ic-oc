89a2647e190c1e8bb2d963d90c7af88a1e09e5be
HisenXuEdu <2230728@tongji.edu.cn>
2025-04-03 15:41:59 +0800

    改恒力追踪，加入原始变阻尼的版本

--- demo/env.py
+++ demo/env.py
@@ -27,7 +27,7 @@ class Env():
         self.k = 5000
 
     def gen_f(self, x, y):
-        ex = self.func_flat(y)
+        ex = self.func_env_scope(y)
         if(x > ex):
             return 0
         return self.k * (ex - x)
@@ -36,7 +36,7 @@ class Env():
         return 0.1 * np.sin(x)
     
     def func_env_scope(self, x):
-        return 0.1 * x
+        return 0.05 * x
 
     def func_env_random(self, x):
         return 0.15  * np.sin(x) * np.exp(-0.2*x)
@@ -61,17 +61,23 @@ class Env():
 
 n = 1000
 env = Env()
-ic = IC(initial_pose=[0, 0, 0.2, 0, 0, 0], k=[128, 128, 0, 5, 5, 5], d=[32, 25, 150, 12, 12, 12], forward_force=np.array([0, 0, des_force, 0, 0, 0]))
-
+ic = IC(initial_pose=[0, 0, 0.2, 0, 0, 0], k=[128, 128, 0, 5, 5, 5], d=[32, 25, 150, 12, 12, 12], forward_force=np.array([0, 0, des_force, 0, 0, 0]), loop_rate=100)
+ic_c = IC(initial_pose=[0, 0, 0.2, 0, 0, 0], k=[128, 128, 0, 5, 5, 5], d=[32, 25, 150, 12, 12, 12], forward_force=np.array([0, 0, des_force, 0, 0, 0]), loop_rate=100)
+ic_raw = IC(initial_pose=[0, 0, 0.2, 0, 0, 0], k=[128, 128, 0, 5, 5, 5], d=[32, 25, 150, 12, 12, 12], forward_force=np.array([0, 0, des_force, 0, 0, 0]), loop_rate=100)
+ 
 pose = [0, 0, 0.01]
 pose_c = [0, 0, 0.01]
+pose_raw = [0, 0, 0.01]
 pose_list = np.empty((n, 3))
 pose_list_c = np.empty((n, 3))
+pose_list_raw = np.empty((n, 3))
 
 force = np.zeros(6)
 force_c = np.zeros(6)
+force_raw = np.zeros(6)
 force_list = np.empty((n, 6))
 force_list_c = np.empty((n, 6))
+force_list_raw = np.empty((n, 6))
 
 record = threading.Thread(target=plot_viz)
 record.daemon = True
@@ -79,17 +85,31 @@ record.start()
 
 y = 0
 for i in range(n):
+
+    # 模拟0.02s的延迟
+    
+    
+
     y += 0.01
     force[2] = env.gen_f(pose[2], y)
+    # force[2] = 0 if i < 3 else env.gen_f(pose_list[i-2][2], y)
     # 传输力的深拷贝
     pose, euler = ic.compute_admittance_env(copy.deepcopy(force))
     pose_list[i] = pose
     force_list[i] = force
 
     force_c[2] = env.gen_f(pose_c[2], y)
-    pose_c, euler = ic.compute_admittance_ff(copy.deepcopy(force))
+    # force_c[2] = 0 if i < 3 else env.gen_f(pose_list_c[i-2][2], y)
+    pose_c, euler = ic_c.compute_admittance_ff(copy.deepcopy(force_c))
     pose_list_c[i] = pose_c
     force_list_c[i] = force_c
+
+    force_raw[2] = env.gen_f(pose_raw[2], y)
+    # force_raw[2] = 0 if i < 3 else env.gen_f(pose_list_raw[i-2][2], y)
+    pose_raw, euler = ic_raw.compute_admittance_env_raw(copy.deepcopy(force_raw))
+    pose_list_raw[i] = pose_raw
+    force_list_raw[i] = force_raw
+
     # sleep(0.002)
 
 # 结束线程
@@ -98,8 +118,8 @@ stop_thread = True
 # 绘制图像
 import matplotlib.pyplot as plt
 plt.figure(figsize=(7, 3.5))
-# plt.plot(np.arange(n)/100, pose_list[:,2], label=r'$x_c$', color='blue', linewidth=1.5)
-plt.plot(np.arange(n)/100, pose_list_c[:,2], label=r'$x_c$', color='green', linewidth=1.5)
+plt.plot(np.arange(n)/100, pose_list[:,2], label=r'$x_c$', color='blue', linewidth=1.5)
+# plt.plot(np.arange(n)/100, pose_list_c[:,2], label=r'$x_c$', color='green', linewidth=1.5)
 # 绘制环境曲线
 plt.plot(np.arange(n)/100, [env.func_flat(0.01*i) for i in range(n)], label=r'$x_e$', color='r', linestyle='--', linewidth=1.5)
 # 添加网格线
@@ -114,7 +134,11 @@ plt.ylabel("Pose(m)", fontsize=18, labelpad=10)
 plt.legend(fontsize=13)
 # 刻度字体大小
 plt.xticks(fontsize=15)  # X 轴刻度
-plt.yticks(fontsize=15)  # Y 轴刻度
+# 设置刻度间隔
+# plt.yticks(np.arange(-0.1, 0.8, step=0.1))  # Y 轴刻度间隔为 0.1
+plt.yticks(fontsize=15, )  # Y 轴刻度
+
+# plt.ylim(-0.12, 0.32)
 
 plt.tight_layout()
 
@@ -156,8 +180,9 @@ plt.tight_layout()
 
 
 plt.figure(figsize=(7, 3.5))
-# plt.plot(np.arange(n)/100, force_list[:,2], label=r'$F_e$', color='blue', linewidth=1.5)
-plt.plot(np.arange(n)/100, force_list_c[:,2], label=r'$F_c$', color='green', linewidth=1.5)
+plt.plot(np.arange(n)/100, force_list[:,2], label=r'$F_e$', color='blue', linewidth=1.5)
+# plt.plot(np.arange(n)/100, force_list_raw[:,2], label=r'$F_r$', color='orange', linewidth=1.5)
+# plt.plot(np.arange(n)/100, force_list_c[:,2], label=r'$F_c$', color='green', linewidth=1.5)
 # plt.ylim(0, 100)
 # 绘制期望力虚线
 plt.plot(np.arange(n)/100, np.ones(n) * des_force, label=r'$F_d$', color = 'r', linestyle='--', linewidth=1.5)
@@ -168,6 +193,13 @@ plt.grid(color='gray', linestyle='--', linewidth=0.5)
 # plt.title(r'Force', fontsize=18)
 plt.xlabel("Time(s)", fontsize=16)
 plt.ylabel("Force(N)", fontsize=16, labelpad=10)
+
+plt.yticks(np.arange(-0, 50, step=10))
+plt.ylim(-2, 25)
+
+# 计算力峰值
+print(np.max(force_list[:,2]))
+print(np.max(force_list_raw[:,2]))
 
 # 添加图例
 plt.legend(fontsize=14)
--- ic/ic.py
+++ ic/ic.py
@@ -6,7 +6,7 @@ import math
 
 class IC():
     def __init__(self, m=[2, 2, 2, 2, 2, 2], d=[32, 25, 12, 12, 12, 12], k=[128, 128, 100, 5, 5, 5],
-                    initial_pose = [138.360397/1000,-472.066620/1000,407.361847/1000, 179.488663,0.264109,179.605057], forward_force=np.zeros(6)):
+                    initial_pose = [138.360397/1000,-472.066620/1000,407.361847/1000, 179.488663,0.264109,179.605057], forward_force=np.zeros(6), loop_rate=75):
 
         # admittance parameters
         self.M_ = np.diag(m)
@@ -32,7 +32,7 @@ class IC():
 
         # 计算循环的时间间隔
         # loop_rate = 20  # Hz
-        loop_rate = 75 # Hz
+        # loop_rate = 75 # Hz
         self.sec = 1.0 / loop_rate
 
         self.last_error = np.zeros(6)
@@ -131,7 +131,34 @@ class IC():
         error[0:3] = self.arm_desired_pose_[0:3]  - self.desired_pose_position_
         error[3:6] = self.arm_desired_pose_[3:6] - self.desired_pose_euler_
         coupling_wrench_arm = np.dot(self.D_, self.arm_desired_twist_) + np.dot(self.K_, error)
-        force -= (self.forward_force - self.last_error - 0.1*(error_force - self.le))
+        force -= (self.forward_force - self.last_error - 0.8*(error_force - self.le))
+        arm_desired_accelaration = np.linalg.inv(self.M_) @ (-coupling_wrench_arm + force)
+        self.__limit_acc(arm_desired_accelaration)
+        self.arm_desired_twist_ += arm_desired_accelaration * self.sec  #进行速度迭代并记录
+        # print(self.arm_desired_twist_)
+        # if limit:
+        #     self.__limitv()
+        self.arm_desired_pose_ += self.arm_desired_twist_ * self.sec  #这里应该用arm_desired_twist_+当前速度
+        pose = self.arm_desired_pose_[0:3] 
+        if limit:
+            pose = self.__limit(pose) #这里仅实现了对xyz的限位
+        euler = self.arm_desired_pose_[3:6] 
+        self.last_error = self.last_error + 0.2*error_force
+        self.le = error_force
+        # self.last_error = 0.99*(self.last_error + 0.6*error_force)
+        return pose, euler
+
+
+    def compute_admittance_env_raw(self, force=np.zeros(6),limit=False):
+        error_force = np.zeros(6)
+        if force[2] > 0:
+            error_force = force - self.forward_force
+
+        error = np.zeros(6)
+        error[0:3] = self.arm_desired_pose_[0:3]  - self.desired_pose_position_
+        error[3:6] = self.arm_desired_pose_[3:6] - self.desired_pose_euler_
+        coupling_wrench_arm = np.dot(self.D_, self.arm_desired_twist_) + np.dot(self.K_, error)
+        force -= (self.forward_force - self.last_error)
         arm_desired_accelaration = np.linalg.inv(self.M_) @ (-coupling_wrench_arm + force)
         self.__limit_acc(arm_desired_accelaration)
         self.arm_desired_twist_ += arm_desired_accelaration * self.sec  #进行速度迭代并记录
